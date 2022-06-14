647a066178411764437856a9a2b5f2770c0a6405
nastia <nastia.bigun@gmail.com>
2022-06-14 16:13:34 -0600

    data types

---
+++ iprep_EricT/prob_20.js
@@ -0,0 +1,9 @@
+// Problem 20
+// what is logged out?
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+
+console.log([] instanceof Array); // true  
