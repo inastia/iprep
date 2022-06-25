c8bd6b37a25e0229e73e8f3813fdf32968e6207e
nastia <nastia.bigun@gmail.com>
2022-06-25 17:39:35 -0600

    folder set up

---
+++ iprep_LeetCode/medium/2.js
@@ -0,0 +1,9 @@
+// Problem 2: Add two numbers
+// You are given two non-empty linked lists representing
+// two non-negative integers. The digits are stored in reverse order,
+// and each of their nodes contains a single digit.
+// Add the two numbers and return the sum as a linked list.
+
+var addTwoNumbers = function(l1, l2) {
+
+};
