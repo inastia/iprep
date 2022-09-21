3b2b1c0d0148941cf69f3f743936324ec8d2a1dd
nastia <nastia.bigun@gmail.com>
2022-09-21 15:28:16 -0600

    leetcode

--- iprep_LeetCode/easy/118.js
+++ iprep_LeetCode/easy/118.js
@@ -17,5 +17,26 @@ Output: [[1]]
 */
 
 var generate = function(numRows) {
+  let result = [[1]];
 
+  // to generate rows
+    for (let row = 1; row < numRows; row++) {
+      result.push([]);
+
+      for (let column = 0; column <= row; column++) {
+        // take adventage of indeces
+        // index of 1st value in every row is 0
+        // index of last value in every row is equal to index of the row itself
+        if (column === 0 || column === row) {
+          result[row][column] = 1;
+        } else {
+        // calculate elements of a row, add each pair of
+        // adjacent elements of the previous row in each
+        // step of the inner loop
+          result[row][column] =
+            result[row - 1][column - 1] + result[row - 1][column];
+        }
+      }
+    }
+	return result;
 };
--- iprep_LeetCode/easy/14.js
+++ iprep_LeetCode/easy/14.js
@@ -8,8 +8,8 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-
   let prefix = '';
+  
   for (let i = 0; i < strs[0].length; i++) {
     let char = strs[0][i];
     for (let j = 1; j < strs.length; j++) {
